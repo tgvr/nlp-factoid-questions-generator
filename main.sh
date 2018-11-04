c86963bff0afc83d4fb719724078aa0ecb817f0c
Gavara Tarun <tarungavara@gmail.com>
2018-11-05 04:31:30 +0530

    Update Tregex for marking unmovable phrases

--- main.sh
+++ main.sh
@@ -5,3 +5,5 @@
 # 3.3.1 - Marking Unmovable Phrases
 python ./python/outputParseTree.py "$1" > ./txt/parseTree.txt
 ./tsurgeon.sh -treeFile ./txt/parseTree.txt ./tsurgeon/3.3.1_mark_unmv > ./txt/3.3.1.txt
+# ./tsurgeon.sh -treeFile ./txt/parseTree.txt ./tsurgeon/test > ./txt/3.3.1.txt
+# cat ./txt/3.3.1.txt
